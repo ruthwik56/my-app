0f99ad096b6dd184bafc922ece73f11b7ac239c4
ruthwik56 <sairuthwik1119@gmail.com>
2024-03-22 12:48:39 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/Homepage.dart';
+import 'package:myapp/HomePage.dart';
 import 'package:myapp/widgets/cartitem.dart';
 import 'package:provider/provider.dart';
 
@@ -23,8 +23,38 @@ class MyApp extends StatelessWidget{
         scaffoldBackgroundColor: Color.fromARGB(255, 230, 227, 227)
       ),
       routes: {
-        "/" :(context)=> HomePage(),
+        "/" :(context)=> SplashScreen(),
       },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Add a delay to simulate a splash screen duration
+    Future.delayed(Duration(seconds: 2),(){
+      // Navigate to the main screen
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage(),)
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        // Display your splash screen image here
+        child: Image.asset('images/Applogo.jpeg'),
+      ),
     );
   }
 }
